f7025cba26116d087b6663c1d632d6232d564d26
Rajashekhar-Biradar <rajashekharbiradar41@gmail.com>
2022-10-14 12:31:03 +0530

    find the largest number

---
+++ FindLargeNum.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day5BasicCoreProblems
+{
+    internal class FindLargeNum
+    {
+        public static void CheckLargestNum()
+        {
+            Console.WriteLine("Enter the three numbers");
+
+            int num1= Convert.ToInt32(Console.ReadLine());
+            int num2= Convert.ToInt32(Console.ReadLine());
+            int num3= Convert.ToInt32(Console.ReadLine());
+
+            if (num1 > num2 && num1 > num3)
+            {
+                Console.WriteLine(" {0} : number is greatest among three",num1);
+            }else if (num2 > num1 && num2 > num3)
+            {
+                Console.WriteLine(" {0} : number is greatest among three",num2);
+            }
+            else
+            {
+                Console.WriteLine(" {0} : number is greatest among three",num3);
+            }
+
+
+        }
+    }
+}
