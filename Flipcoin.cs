575feb6e6fbec10bd36ed0d1fffaf9ec589d1876
Rajashekhar-Biradar <rajashekharbiradar41@gmail.com>
2022-10-14 12:13:28 +0530

    find the percentage of flip coin and Leap year for given year

---
+++ Flipcoin.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCoreProblems
+{
+    internal class Flipcoin
+    {
+       
+
+        public static void CheckPerOfFlipcoin()
+        {
+            Console.WriteLine("enter the number of times to flip coin");
+            int n = Convert.ToInt32(Console.ReadLine());
+            int Tail=0;
+            int Head=0;
+            
+            for (int i = 0; i < n; i++)
+            {
+                Random random = new Random();
+                double check = random.NextDouble();
+               
+                if (check < 0.5)
+                {
+                    Console.WriteLine("Tails");
+                    Tail++;
+                }
+                else
+                {
+                    Console.WriteLine("Heads");
+                    Head++;
+                } 
+
+            }
+            Console.WriteLine("number of head counts"+ Head);
+            Console.WriteLine("number of tails counts"+ Tail);
+            //double a = (Head/n)*100;
+            //double b = Tail/n*100;
+            double a = (Head*100)/n;
+            double b = (Tail*100)/n;
+            Console.WriteLine("Percentage Of Head is : " + a);
+            Console.WriteLine("Percentage Of Tail is : " + b);
+
+
+        }
+           
+    }
+        
+}
---
+++ LeapYear.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCoreProblems
+{
+    internal class LeapYear
+    {
+        public static void CheckLeapYear()
+        {
+            Console.WriteLine("Enter the year to check leap year");
+            int year =Convert.ToInt32(Console.ReadLine());
+
+            while(year < 1000)
+            {
+                Console.WriteLine(" enter valid 4 digit number ");
+                year = Convert.ToInt32(Console.ReadLine());
+            }
+
+            if (year % 4 == 0)
+            {
+                Console.WriteLine("The year is a Leap year");
+            }
+            else
+            {
+                Console.WriteLine("The year is not a Leap year");
+            }
+        }
+
+        
+          
+
+    }
+}
