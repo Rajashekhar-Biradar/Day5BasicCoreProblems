1a06dd9e9c31144652f017d47f0dd76e9517eafb
Rajashekhar-Biradar <rajashekharbiradar41@gmail.com>
2022-10-14 12:14:39 +0530

    find the power of 2 for nth number

---
+++ PowerOf2.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Transactions;
+
+namespace Day5BasicCoreProblems
+{
+    public class PowerOf2
+    {
+        public static void PowerOfTwo()
+        {
+            Console.WriteLine("enter a number");
+            
+                int num = Convert.ToInt32(Console.ReadLine());
+                
+
+            if (num > 31)
+            {
+                Console.WriteLine("enter valid number");
+            }
+            else
+            {
+                for (int i = 0; i <= num; i++)
+                {
+                    
+                    Console.WriteLine("Power of two is : "+ Math.Pow(2,i));   
+                }
+                
+            }
+            
+
+           
+              
+            
+            
+        }
+    }
+}
