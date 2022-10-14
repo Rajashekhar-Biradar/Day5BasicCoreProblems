c8aaa64d671e889e0fd3a0d12db498bfc49b2643
Rajashekhar-Biradar <rajashekharbiradar41@gmail.com>
2022-10-14 12:35:15 +0530

    main method

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using BasicCoreProblems;
+using Day5BasicCoreProblems;
+
+
+//Flipcoin.CheckPerOfFlipcoin();
+//LeapYear.CheckLeapYear();
+//PowerOf2.PowerOfTwo();
+//HarmonicNumber.Harmonic_num();
+//Quotient_and_Remainder.CheckQuotientAndRemainder();
+//Swap.SwapTwoNo();
+//CheckEvenOdd.CheckTheNum();
+//CheckAlphabet.CheckAlphabets();
+//FindLargeNum.CheckLargestNum();
+PrimeFactor.primeFactors(100);
