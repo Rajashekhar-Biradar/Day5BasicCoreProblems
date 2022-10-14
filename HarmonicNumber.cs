517e3141c2ae6e9651d4d15ccef7513c533ddcf6
Rajashekhar-Biradar <rajashekharbiradar41@gmail.com>
2022-10-14 12:21:11 +0530

    find the harmonic sum of nth number

---
+++ HarmonicNumber.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day5BasicCoreProblems
+{
+    public class HarmonicNumber
+    {
+        public static void Harmonic_num()
+        {
+            double harmonicSum=0;
+            Console.WriteLine("enter the n^th number");
+            int num=Convert.ToInt32(Console.ReadLine());
+
+            for (double i = 1; i <= num; i++)
+            {
+               Console.WriteLine("1/{0}",i);
+                harmonicSum += (1/i);
+            }
+            Console.WriteLine("total harmonic sum : " + harmonicSum);
+        }
+        
+    }
+}
